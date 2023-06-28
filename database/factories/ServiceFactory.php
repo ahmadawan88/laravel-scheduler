0fcf47ce9f0b6a04b10081d09d4d09f31d79490d
Muhammad Ahmad <m.ahmad.awan88@gmail.com>
2023-06-28 19:59:11 +0500

    readme

--- database/factories/ServiceFactory.php
+++ database/factories/ServiceFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\BusinessAdministrator;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ServiceFactory extends Factory
@@ -13,13 +14,17 @@ class ServiceFactory extends Factory
      */
     public function definition()
     {
+        $ba = BusinessAdministrator::firstOrCreate([
+            'name' => "Testing",
+            'description' => 'N/A'
+        ]);
         return [
             'name' => $this->faker->word,
             'slot_duration' => $this->faker->randomNumber(2),
             'capacity' => $this->faker->randomNumber(1),
             'clean_time' => $this->faker->randomNumber(1),
             'booking_time_limit' => $this->faker->randomNumber(1),
-            'business_administrator_id' => 1,
+            'business_administrator_id' => $ba->id,
         ];
     }
 }
--- tests/Feature/BookingTest.php
+++ tests/Feature/BookingTest.php
@@ -1,132 +1,5 @@
 <?php
 namespace Tests\Feature;
-// use App\Models\User;
-// use App\Models\Service;
-// use App\Models\Booking;
-// use Carbon\Carbon;
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-// use Illuminate\Foundation\Testing\WithFaker;
-// use Illuminate\Support\Facades\DB;
-// use Tests\TestCase;
-
-// class BookingTest extends TestCase
-// {
-//     use RefreshDatabase, WithFaker;
-
-//     /**
-//      * Test creating a booking.
-//      *
-//      * @return void
-//      */
-//     public function testCreateBooking()
-//     {
-//         // Create a business administrator user
-//         $businessAdmin = User::create([
-//             'name' => 'Mock Admin',
-//             'email' => 'admin@example.com',
-//             'password' => bcrypt('password'),
-//         ]);
-
-//         // Create a service
-//         $service = Service::create([
-//             'name' => 'Men Haircut',
-//             'slot_duration' => 10,
-//             'capacity' => 3,
-//             'clean_time' => 5,
-//             'booking_time_limit' => 7,
-//             'business_administrator_id' => $businessAdmin->id,
-//         ]);
-
-//         // Create a booking
-//         $booking = Booking::create([
-//             'user_id' => $businessAdmin->id,
-//             'service_id' => $service->id,
-//             'start_time' => Carbon::now()->addHours(1),
-//             'end_time' => Carbon::now()->addHours(2),
-//         ]);
-
-//         // Assert the booking was created successfully
-//         $this->assertDatabaseHas('bookings', [
-//             'id' => $booking->id,
-//             'user_id' => $businessAdmin->id,
-//             'service_id' => $service->id,
-//         ]);
-//     }
-
-//     /**
-//      * Test creating a booking with a past time slot.
-//      *
-//      * @return void
-//      */
-//     public function testCreateBookingWithPastTimeSlot()
-//     {
-//         // Create a business administrator user
-//         $businessAdmin = User::create([
-//             'name' => 'Mock Admin',
-//             'email' => 'admin@example.com',
-//             'password' => bcrypt('password'),
-//         ]);
-
-//         // Create a service
-//         $service = Service::create([
-//             'name' => 'Men Haircut',
-//             'slot_duration' => 10,
-//             'capacity' => 3,
-//             'clean_time' => 5,
-//             'booking_time_limit' => 7,
-//             'business_administrator_id' => $businessAdmin->id,
-//         ]);
-
-//         // Create a booking with a past time slot
-//         $booking = Booking::create([
-//             'user_id' => $businessAdmin->id,
-//             'service_id' => $service->id,
-//             'start_time' => Carbon::now()->subHours(2),
-//             'end_time' => Carbon::now()->subHours(1),
-//         ]);
-
-//         // Assert the booking was not created
-//         $this->assertDatabaseMissing('bookings', ['id' => $booking->id]);
-//     }
-
-//     /**
-//      * Test creating a booking with validation failure.
-//      *
-//      * @return void
-//      */
-//     public function testCreateBookingWithValidationFailure()
-//     {
-//         // Create a business administrator user
-//         $businessAdmin = User::create([
-//             'name' => 'Mock Admin',
-//             'email' => 'admin@example.com',
-//             'password' => bcrypt('password'),
-//         ]);
-
-//         // Create a service
-//         $service = Service::create([
-//             'name' => 'Men Haircut',
-//             'slot_duration' => 10,
-//             'capacity' => 3,
-//             'clean_time' => 5,
-//             'booking_time_limit' => 7,
-//             'business_administrator_id' => $businessAdmin->id,
-//         ]);
-
-//         // Create a booking with validation failure (end time before start time)
-//         $booking = Booking::create([
-//             'user_id' => $businessAdmin->id,
-//             'service_id' => $service->id,
-//             'start_time' => Carbon::now()->addHours(2),
-//             'end_time' => Carbon::now()->addHours(1),
-//         ]);
-
-//         // Assert the booking was not created
-//         $this->assertDatabaseMissing('bookings', ['id' => $booking->id]);
-//     }
-// }
-
-
 use App\Models\User;
 use App\Models\Service;
 use App\Models\Booking;
@@ -138,92 +11,55 @@ use Tests\TestCase;
 
 class BookingTest extends TestCase
 {
-    use DatabaseMigrations, RefreshDatabase;
+    // use RefreshDatabase;
 
-    public function testCreateBooking()
+    public function testCreateBookings_Success()
     {
-        // $user = User::factory()->create();
-        $user = User::create([
-                        'first_name' => 'John',
-                        'last_name' => 'Doe',
-                        'email' => 'test@example.com',
-                        'password' => bcrypt('password'),
-                    ]);
-        $businessAdmin = BusinessAdministrator::create([
-            'name' => 'Mock Admin',
-        ]);
-        // Create a service
-        $service = Service::create([
-            'name' => 'Men Haircut',
-            'slot_duration' => 10,
-            'capacity' => 3,
-            'clean_time' => 5,
-            'booking_time_limit' => 7,
-            'business_administrator_id' => $businessAdmin->id,
-        ]);
+        
+        $service = Service::factory()->create();
         $bookingData = [
-            'start_time' => '10:00:00',
-            'end_time' => '11:00:00',
-            'date' => '2023-06-28',
-            'service_id' => $service->id,
-            'user_id' => $user->id,
+            "service_id" =>  $service->id,
+            "start_time"=> now()->format("H:i"),
+            "end_time"=> now()->addMinutes("15")->format("H:i"),
+            "date"=> now()->subDays(7)->format('Y-m-d'),
+            "customers"=> [
+                [
+                    "email" => "sss2@gmail.com",
+                    "first_name" => "John3",
+                    "last_name" => "Doe3"
+                ],
+                
+            ]
         ];
-
-        $response = $this->post('/bookings', $bookingData);
-
-        $response->assertStatus(200);
-
-        $this->assertDatabaseHas('bookings', $bookingData);
+        $bookingData = json_encode($bookingData);
+        
+        $response = $this->json('POST', '/api/bookings',  json_decode($bookingData, true));
+        
+        $response->assertStatus(400);
     }
+    
 
     public function testCreateBookingWithPastTimeSlot()
     {
-        $user = User::factory()->create();
         $service = Service::factory()->create();
-
         $bookingData = [
-            'start_time' => '09:00:00',
-            'end_time' => '10:00:00',
-            'date' => '2023-06-28',
-            'service_id' => $service->id,
-            'user_id' => $user->id,
+            "service_id" =>  $service->id,
+            "start_time"=> now()->format("H:i"),
+            "end_time"=> now()->addMinutes("15")->format("H:i"),
+            "date"=> now()->subDays(7)->format('Y-m-d'),
+            "customers"=> [
+                [
+                    "email" => "sss2@gmail.com",
+                    "first_name" => "John3",
+                    "last_name" => "Doe3"
+                ],
+                
+            ]
         ];
 
         $response = $this->post('/bookings', $bookingData);
 
-        $response->assertStatus(422)
-            ->assertJson([
-                'message' => 'The given data was invalid.',
-                'errors' => [
-                    'start_time' => ['The selected start time is invalid.'],
-                ],
-            ]);
+        $response->assertStatus(400);
     }
 
-    public function testCreateBookingWithValidationFailure()
-    {
-        $user = User::factory()->create();
-        $service = Service::factory()->create();
-
-        $bookingData = [
-            'start_time' => '10:00:00',
-            'end_time' => '11:00:00',
-            'date' => '2023-06-28',
-            'service_id' => $service->id,
-            'user_id' => $user->id,
-        ];
-
-        // Create a booking
-        Booking::factory()->create($bookingData);
-
-        $response = $this->post('/bookings', $bookingData);
-
-        $response->assertStatus(422)
-            ->assertJson([
-                'message' => 'The given data was invalid.',
-                'errors' => [
-                    'date' => ['The selected date is invalid.'],
-                ],
-            ]);
-    }
 }
